f07187b1e354802a142b7d1c8360dfae49e1c130
Christian Simon <csimon1127@gmail.com>
2020-10-05 19:15:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,8 +9,11 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    
-    
   end 
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.add_song(song)
+  end
   
 end
