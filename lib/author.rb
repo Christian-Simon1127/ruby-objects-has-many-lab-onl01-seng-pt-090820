1e979aab74f2f9f994000da8cf48b51819060d32
Christian Simon <csimon1127@gmail.com>
2020-10-05 19:28:33 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,24 @@
+class Author 
+  attr_accessor :name, :posts
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post1)
+    @posts << post1
+    post.artist = self
+  end 
+  
+  def add_post_by_title(name)
+    post = Post.new(name)
+    self.add_post(post1)
+  end
+  
+  def self.post_count()
+    Post.all.length
+  end 
+  
+  
+end
